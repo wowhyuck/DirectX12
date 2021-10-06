b60fcab0bd7e0712fa60aa15a54b3ed116efaf36
Joohyuck <joohyuck.seo@gmail.com>
2021-10-06 13:45:06 +0900

    삼각형 띄우기

--- Engine/CommandQueue.h
+++ Engine/CommandQueue.h
@@ -8,13 +8,14 @@ class CommandQueue
 public:
 	~CommandQueue();
 
-	void Init(ComPtr<ID3D12Device> device, shared_ptr<SwapChain> swapChain, shared_ptr<DescriptorHeap> descHeap);;
+	void Init(ComPtr<ID3D12Device> device, shared_ptr<SwapChain> swapChain);
 	void WaitSync();
 
 	void RenderBegin(const D3D12_VIEWPORT* vp, const D3D12_RECT* rect);
 	void RenderEnd();
 	
 	ComPtr<ID3D12CommandQueue> GetCmdQueue() { return _cmdQueue; }
+	ComPtr<ID3D12GraphicsCommandList> GetCmdList() { return	_cmdList; }
 
 private:
 	// CommandQueue : DX12에 등장
@@ -31,6 +32,5 @@ private:
 	HANDLE								_fenceEvent = INVALID_HANDLE_VALUE;
 	
 	shared_ptr<SwapChain>				_swapChain;
-	shared_ptr<DescriptorHeap>			_descHeap;
 };
 
